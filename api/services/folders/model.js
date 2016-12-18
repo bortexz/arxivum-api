0976e74a0af21db516828bdcd295734ed18fc67f
Alberto Fernandez <alberto.fernandez.491@gmail.com>
2016-12-18 13:00:17 +0000

    removed unused function

--- api/services/folders/model.js
+++ api/services/folders/model.js
@@ -10,15 +10,8 @@ const folderSchema = new Schema({
   description: String
 })
 
-folderSchema.methods.getPath = getPath
 folderSchema.plugin(tree)
 
 const Folder = mongoose.model('Folder', folderSchema)
 
 module.exports = Folder
-
-function getPath () {
-  // this === folder I am -> Populate all the way? Will I have it populated already?
-  // Do tests. Do navigate in tree for admin in FE, can test this better when I Will
-  // be able to create trees.
-}
