3c5edfdc83e0582e916fa4bbf9b1fb7de246300b
Alberto Fernandez <alberto.fernandez.491@gmail.com>
2017-05-09 09:11:59 +0100

    feat: add error constant creators

--- api/services/files/errors.js
+++ api/services/files/errors.js
@@ -11,10 +11,13 @@ const SAVE_DATABASE_ERROR =
 
 const GENERATE_CRYPTO_KEY_ERROR = 'Error while generating cryptographic key'
 
+const FILE_NOT_FOUND_ERROR = (params) => `File not found: ${params}`
+
 module.exports = {
   ENCRYPTION_ERROR,
   FILESYSTEM_SAVE_ERROR,
   GENERATE_TORRENT_ERROR,
   SAVE_DATABASE_ERROR,
-  GENERATE_CRYPTO_KEY_ERROR
+  GENERATE_CRYPTO_KEY_ERROR,
+  FILE_NOT_FOUND_ERROR
 }
---
+++ api/services/folders/errors.js
@@ -0,0 +1,6 @@
+const GET_CHILDREN_TREE_ERROR =
+  (params) => `Error getting children tree of: ${params}`
+
+module.exports = {
+  GET_CHILDREN_TREE_ERROR
+}
