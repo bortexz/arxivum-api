01cf5b263910ec562af39a015ac74b1abb907190
Alberto Fernandez <alberto.fernandez.491@gmail.com>
2017-06-13 11:36:59 +0100

    removed ENV variables from dev environment

--- api/config/dev.config.js
+++ api/config/dev.config.js
@@ -4,36 +4,39 @@ const config = {
   // Main url for the application. If the others are not specified,
   // This one will be taken adding prefixes (/tracker, /api,...) no prefix is
   // front end app
-  PUBLIC_URL: process.env.PUBLIC_URL || 'localhost: 4200',
+  PUBLIC_URL: 'localhost:4200',
 
   // Database options
-  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/arxivum-dev',
+  DATABASE_URL: 'mongodb://localhost/arxivum-dev',
 
-  // Initial admin email for the database. Move to first migration ?
-  ADMIN_EMAIL: process.env.ADMIN_EMAIL || 'admin@admin',
-  ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || 'admin',
+  // Initial admin email for the database.
+  ADMIN_EMAIL: 'admin@admin',
+  ADMIN_PASSWORD: 'admin',
 
   // Security options
   // JWT_SECRET: process.env.JWT_SECRET || 's0m3th1nG_R4nd0M' + Math.round(Math.random() * 100),
-  JWT_SECRET: process.env.JWT_SECRET || 's0m3th1nG_R4nd0M',
+  JWT_SECRET: 's0m3th1nG_R4nd0M',
 
   // Webseed options (Maybe upload to webseed directly ?)
-  WEBSEED_FOLDER:
-    process.env.WEBSEED_FOLDER || path.resolve(__dirname, '../../files'),
+  WEBSEED_FOLDER: path.resolve(__dirname, '../../files'),
 
   // Public url where to access this api
-  PUBLIC_API_URL: process.env.PUBLIC_URL || 'http://localhost:2000/',
+  // Public url where to access this api
+  PUBLIC_API_URL: 'http://localhost:2000/',
 
-  // Where the tracker is
-  PUBLIC_TRACKER_URL: process.env.PUBLIC_TRACKER_URL || 'ws://localhost:2000',
+  // Where the tracker is (WebSocket tracker)
+  PUBLIC_TRACKER_URL: 'ws://localhost:2000/tracker',
+
+  // Where the webseed is
+  PUBLIC_WEBSEED_URL: 'ws://localhost:2000/webseed',
 
   // Nodemailer options
   // For now only this ones are supported
   // https://nodemailer.com/smtp/well-known/
-  EMAILER_SMTP_SERVICE: process.env.EMAILER_SMTP_SERVICE || 'test',
-  EMAILER_PUBLIC_EMAIL: process.env.EMAILER_PUBLIC_USER || 'test@test',
-  EMAILER_AUTH_USER: process.env.EMAILER_AUTH_EMAIL || 'test@test',
-  EMAILER_AUTH_PASSWORD: process.env.EMAILER_AUTH_PASSWORD || 'test'
+  EMAILER_SMTP_SERVICE: 'test',
+  EMAILER_PUBLIC_EMAIL: 'test@test',
+  EMAILER_AUTH_USER: 'test@test',
+  EMAILER_AUTH_PASSWORD: 'test'
 }
 
 module.exports = config
